32616c97b8e828153ce9053a3402a60d7f379bc1
Abdulrahman Wael <121362133+AbdulrahmanWael1@users.noreply.github.com>
2022-12-25 00:35:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, currentNum, devisor, sum;
+
+            Console.WriteLine("Enter the first number");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            num2 = int.Parse(Console.ReadLine());
+
+            for (currentNum = num1; currentNum <= num2; currentNum++)
+            {
+                sum = 0;
+
+                for (devisor = 1; devisor <= currentNum; devisor++)
+                {
+                    if (currentNum%devisor == 0)
+                    {
+                        sum++;
+                    }
+                }
+
+                if (sum == 2)
+                {
+                    Console.WriteLine(currentNum);
+                }
+            }
+        }
+    }
+}
